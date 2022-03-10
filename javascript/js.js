ac1078c33d69779b9b678b48c097bf5ae08d06c7
Akshansh Chauhan <92544823+AkshanshChauhan@users.noreply.github.com>
2022-03-10 23:07:40 +0530

    Add files via upload

---
+++ javascript/js.js
@@ -0,0 +1,10 @@
+function opan_dash() {
+    document.getElementById("dash").style.display = "flex";
+    document.getElementById("table").style.display = "none";
+}
+
+function open_table() {
+    document.getElementById("table").style.display = "flex";
+    document.getElementById("dash").style.display = "none";
+}
+
