98a2aba043b555b66e12f99a52f4ae132289c941
Akshansh Chauhan <92544823+AkshanshChauhan@users.noreply.github.com>
2022-03-11 13:31:28 +0530

    Update js.js

--- javascript/js.js
+++ javascript/js.js
@@ -8,3 +8,23 @@ function open_table() {
     document.getElementById("dash").style.display = "none";
 }
 
+function search(value) {
+    var textvalue, suggest;
+    var filter = value.toUpperCase();
+    var sugbox = document.getElementById("show");
+    var li = sugbox.getElementsByTagName("li");
+    for(let i=0; i<li.length; i++) {
+        suggest = li[i].getElementsByTagName("a")[0];
+        textvalue = suggest.textContent || suggest.innerText;
+        if(textvalue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "grid";
+        } else {
+            li[i].style.display = "none";
+        }
+        if(value == "") {
+            sugbox.style.display = "none";
+        } else {
+            sugbox.style.display = "grid";
+        }
+    }
+}
